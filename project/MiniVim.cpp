6a661c3057c0b3ca1076090b8bbcb3faf9351501
Zmjjeff7 <2519416535@qq.com>
2024-12-06 16:33:35 +0800

    Update MiniVim.cpp

--- project/MiniVim.cpp
+++ project/MiniVim.cpp
@@ -14,12 +14,12 @@ public:
         while (true) {
             draw();
             char ch = getch();
-            if (ch == 'q') break;
+            if (ch == 'q') break; // 临时退出键
             if(command_mode_active){
-                //TODO
+                command_mode(ch);
             }
             else if(insert_mode_active){
-                //TODO
+                insert_mode(ch);
             }
             else{
                 normal_mode(ch);
@@ -74,20 +74,60 @@ private:
             case 'h':
                 if (cursor_x > 0) --cursor_x;
                 break;
+            case KEY_LEFT:
+                if (cursor_x > 0) --cursor_x;
+                break;
             case 'j':
-                if (cursor_y < lines.size() - 1) ++cursor_y;
+                if (cursor_y < lines.size() - 1 && cursor_x < lines[cursor_y+1].length()) ++cursor_y;
+                else if (cursor_y < lines.size() - 1) {
+                    ++cursor_y;
+                    cursor_x = lines[cursor_y].length();
+                }
+                break;
+            case KEY_DOWN:
+                if (cursor_y < lines.size() - 1 && cursor_x < lines[cursor_y+1].length()) ++cursor_y;
+                else if (cursor_y < lines.size() - 1) {
+                    ++cursor_y;
+                    cursor_x = lines[cursor_y].length();
+                }
                 break;
             case 'k':
-                if (cursor_y > 0) --cursor_y;
+                if (cursor_y > 0 && cursor_x < lines[cursor_y-1].length()) --cursor_y;
+                else if (cursor_y > 0) {
+                    --cursor_y;
+                    cursor_x = lines[cursor_y].length();
+                }
+                break;
+            case KEY_UP:
+                if (cursor_y > 0 && cursor_x < lines[cursor_y-1].length()) --cursor_y;
+                else if (cursor_y > 0) {
+                    --cursor_y;
+                    cursor_x = lines[cursor_y].length();
+                }
                 break;
             case 'l':
                 if (cursor_x < lines[cursor_y].length()) ++cursor_x;
                 break;
+            case KEY_RIGHT:
+                if (cursor_x < lines[cursor_y].length()) ++cursor_x;
+                break;            
             case '0':
                 cursor_x = 0;
                 break;
             case '$':
                 cursor_x = lines[cursor_y].length();
+                break;
+            case 'g':
+                if (getch() == 'g') {
+                    cursor_y = 0;
+                    cursor_x = 0;
+                    ch=0;
+                }
+                else break;
+                break;
+            case 'G':
+                cursor_y = lines.size() - 1;
+                cursor_x = 0;
                 break;
             case 'i':
                 insert_mode_active = true;
@@ -100,9 +140,17 @@ private:
         }
     }
 
-    void insert_mode(int ch){}
+    void insert_mode(int ch){
+        // ch==27 ESC键
+        if (ch==27){
+            insert_mode_active=false;
+        }
+        //TODO
+    }
 
-    void command_mode(int ch){}
+    void command_mode(int ch){
+        //TODO
+    }
 
     void add_char(char c){}
 
